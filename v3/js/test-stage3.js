15d46756e5010d4ed5f2c34c89f77a296727121a
eunahpae <eunahp86@gmail.com>
2026-03-04 18:00:25 +0900

    문제수정

--- v3/js/test-stage3.js
+++ v3/js/test-stage3.js
@@ -12,58 +12,70 @@ const stage3QuestionBank = [
         id: 's3_q1',
         questionImage: './image/stage3/question1.jpg',
         choices: [
-            { id: 1, image: './image/stage2/q1-option1.jpg', isCorrect: true },
-            { id: 2, image: './image/stage2/q1-option2.jpg', isCorrect: false },
-            { id: 3, image: './image/stage2/q1-option3.jpg', isCorrect: false },
-            { id: 4, image: './image/stage2/q1-option4.jpg', isCorrect: false },
-            { id: 5, image: './image/stage2/q1-option5.jpg', isCorrect: false },
-            { id: 6, image: './image/stage2/q1-option6.jpg', isCorrect: false },
-            { id: 7, image: './image/stage2/q1-option7.jpg', isCorrect: false },
-            { id: 8, image: './image/stage2/q1-option8.jpg', isCorrect: false }
+            { id: 1, image: './image/stage3/q1-option1.jpg', isCorrect: true },
+            { id: 2, image: './image/stage3/q1-option2.jpg', isCorrect: false },
+            { id: 3, image: './image/stage3/q1-option3.jpg', isCorrect: false },
+            { id: 4, image: './image/stage3/q1-option4.jpg', isCorrect: false },
+            { id: 5, image: './image/stage3/q1-option5.jpg', isCorrect: false },
+            { id: 6, image: './image/stage3/q1-option6.jpg', isCorrect: false },
+            { id: 7, image: './image/stage3/q1-option7.jpg', isCorrect: false },
+            { id: 8, image: './image/stage3/q1-option8.jpg', isCorrect: false }
         ]
     },
     {
         id: 's3_q2',
         questionImage: './image/stage3/question1.jpg',
         choices: [
-            { id: 1, image: './image/stage2/q1-option1.jpg', isCorrect: true },
-            { id: 2, image: './image/stage2/q1-option2.jpg', isCorrect: false },
-            { id: 3, image: './image/stage2/q1-option3.jpg', isCorrect: false },
-            { id: 4, image: './image/stage2/q1-option4.jpg', isCorrect: false },
-            { id: 5, image: './image/stage2/q1-option5.jpg', isCorrect: false }
+            { id: 1, image: './image/stage3/q1-option1.jpg', isCorrect: true },
+            { id: 2, image: './image/stage3/q1-option2.jpg', isCorrect: false },
+            { id: 3, image: './image/stage3/q1-option3.jpg', isCorrect: false },
+            { id: 4, image: './image/stage3/q1-option4.jpg', isCorrect: false },
+            { id: 5, image: './image/stage3/q1-option5.jpg', isCorrect: false },
+            { id: 6, image: './image/stage3/q1-option6.jpg', isCorrect: false },
+            { id: 7, image: './image/stage3/q1-option7.jpg', isCorrect: false },
+            { id: 8, image: './image/stage3/q1-option8.jpg', isCorrect: false }
         ]
     },
     {
         id: 's3_q3',
         questionImage: './image/stage3/question1.jpg',
         choices: [
-            { id: 1, image: './image/stage2/q1-option1.jpg', isCorrect: true },
-            { id: 2, image: './image/stage2/q1-option2.jpg', isCorrect: false },
-            { id: 3, image: './image/stage2/q1-option3.jpg', isCorrect: false },
-            { id: 4, image: './image/stage2/q1-option4.jpg', isCorrect: false },
-            { id: 5, image: './image/stage2/q1-option5.jpg', isCorrect: false }
+            { id: 1, image: './image/stage3/q1-option1.jpg', isCorrect: true },
+            { id: 2, image: './image/stage3/q1-option2.jpg', isCorrect: false },
+            { id: 3, image: './image/stage3/q1-option3.jpg', isCorrect: false },
+            { id: 4, image: './image/stage3/q1-option4.jpg', isCorrect: false },
+            { id: 5, image: './image/stage3/q1-option5.jpg', isCorrect: false },
+            { id: 6, image: './image/stage3/q1-option6.jpg', isCorrect: false },
+            { id: 7, image: './image/stage3/q1-option7.jpg', isCorrect: false },
+            { id: 8, image: './image/stage3/q1-option8.jpg', isCorrect: false }
         ]
     },
     {
         id: 's3_q4',
         questionImage: './image/stage3/question1.jpg',
         choices: [
-            { id: 1, image: './image/stage2/q1-option1.jpg', isCorrect: true },
-            { id: 2, image: './image/stage2/q1-option2.jpg', isCorrect: false },
-            { id: 3, image: './image/stage2/q1-option3.jpg', isCorrect: false },
-            { id: 4, image: './image/stage2/q1-option4.jpg', isCorrect: false },
-            { id: 5, image: './image/stage2/q1-option5.jpg', isCorrect: false }
+            { id: 1, image: './image/stage3/q1-option1.jpg', isCorrect: true },
+            { id: 2, image: './image/stage3/q1-option2.jpg', isCorrect: false },
+            { id: 3, image: './image/stage3/q1-option3.jpg', isCorrect: false },
+            { id: 4, image: './image/stage3/q1-option4.jpg', isCorrect: false },
+            { id: 5, image: './image/stage3/q1-option5.jpg', isCorrect: false },
+            { id: 6, image: './image/stage3/q1-option6.jpg', isCorrect: false },
+            { id: 7, image: './image/stage3/q1-option7.jpg', isCorrect: false },
+            { id: 8, image: './image/stage3/q1-option8.jpg', isCorrect: false }
         ]
     },
     {
         id: 's3_q5',
         questionImage: './image/stage3/question1.jpg',
         choices: [
-            { id: 1, image: './image/stage2/q1-option1.jpg', isCorrect: true },
-            { id: 2, image: './image/stage2/q1-option2.jpg', isCorrect: false },
-            { id: 3, image: './image/stage2/q1-option3.jpg', isCorrect: false },
-            { id: 4, image: './image/stage2/q1-option4.jpg', isCorrect: false },
-            { id: 5, image: './image/stage2/q1-option5.jpg', isCorrect: false }
+            { id: 1, image: './image/stage3/q1-option1.jpg', isCorrect: true },
+            { id: 2, image: './image/stage3/q1-option2.jpg', isCorrect: false },
+            { id: 3, image: './image/stage3/q1-option3.jpg', isCorrect: false },
+            { id: 4, image: './image/stage3/q1-option4.jpg', isCorrect: false },
+            { id: 5, image: './image/stage3/q1-option5.jpg', isCorrect: false },
+            { id: 6, image: './image/stage3/q1-option6.jpg', isCorrect: false },
+            { id: 7, image: './image/stage3/q1-option7.jpg', isCorrect: false },
+            { id: 8, image: './image/stage3/q1-option8.jpg', isCorrect: false }
         ]
     }
 ];
